aea3cb9e2ef560d749fb9bb0894315edafcca33e
absman-x <amc-x@yandex.ru>
2021-06-02 00:11:36 +0400

    правки после проверки наставником

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 /*
 Взял с https://html5css.ru/js/js_random.php
 */
-function returnRandomNumber (min, max) {
+function getRandomInteger (min, max) {
   if (min > max || min <0) {
     throw new Error ('Минимальное значение должно быть больше нуля и меньше максимального.');
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-returnRandomNumber (1, 5);
+getRandomInteger (1, 5);
 
-//const maxCommentLength = 140;
+//const validLength = 140;
 
-function checkLength (commentText, maxCommentLength) {
-  if (commentText.length > maxCommentLength) {
-    return false;
-  }
-  return true;
+function validateLength(text, validLength) {
+  return text.length <= validLength;
 }
 
-checkLength('тест', 140);
+validateLength('тест', 140);
