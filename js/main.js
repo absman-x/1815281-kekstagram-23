7bb9bf9003b707635fa018d5ad6c617b1ef013a3
absman-x <amc-x@yandex.ru>
2021-06-09 01:13:22 +0400

    added function for user generation

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ function getRandomInteger (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInteger (1, 5);
-
 //const validLength = 140;
 
 function validateLength(text, validLength) {
@@ -17,3 +15,53 @@ function validateLength(text, validLength) {
 }
 
 validateLength('тест', 140);
+
+function getFakeUsers () {
+  const fakeUsers = [];
+  const messageVariants = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  const nameVariants = ['Дмитрий', 'Анатолий', 'Рушат', 'Андрей', 'Ольга', 'Евгений', 'Наталья'];
+  const descriptionFirstPart = ['Просто ', 'Супер ', 'Это '];
+  const descriptionSecondPart = ['фотка', 'фото', 'картинка'];
+  const totallyRandomArray = [];
+
+  function isInArray(array, el) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === el) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  function getUniqueRandomId(array) {
+    const randomId = getRandomInteger(1, 100);
+    if (!isInArray(totallyRandomArray, randomId)) {
+      totallyRandomArray.push(randomId);
+      return randomId;
+    }
+    return getUniqueRandomId(array);
+  }
+
+  function getRandomComment() {
+    const comment = {};
+    comment.id = getUniqueRandomId(totallyRandomArray);
+    comment.avatar = `img/avatar-${getRandomInteger(1, 6)}.svg`;
+    comment.message = messageVariants[getRandomInteger(0, messageVariants.length - 1)];
+    comment.name = nameVariants[getRandomInteger(0, nameVariants.length - 1)];
+    return comment;
+  }
+
+  for (let i = 1; i <= 25; i++) {
+    const currentFakeUser = {};
+    currentFakeUser.id = i;
+    currentFakeUser.url = `photos/${i}.jpg`;
+    currentFakeUser.description = `${descriptionFirstPart[getRandomInteger(0, descriptionFirstPart.length - 1)]}${descriptionSecondPart[getRandomInteger(0, descriptionSecondPart.length - 1)]}`;
+    currentFakeUser.likes = getRandomInteger(15, 200);
+    currentFakeUser.comments = getRandomComment();
+    fakeUsers.push(currentFakeUser);
+  }
+  return fakeUsers;
+}
+
+getFakeUsers();
