b0571afa26a74a8b3aa34ade4f3d526d1e9ded68
absman-x <84252573+absman-x@users.noreply.github.com>
2021-06-11 22:00:30 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ validateLength('тест', 140);
 
 const COUNT_MOCK_USERS = 25;
 const MAX_COMMENTS = 3;
-const USERS = [];
 const MESSAGE_VARIANTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 const NAME_VARIANTS = ['Дмитрий', 'Анатолий', 'Рушат', 'Андрей', 'Ольга', 'Евгений', 'Наталья', 'Вячеслав', 'Альфия'];
 const DESCRIPTION_FIRST_PARTS = ['Просто ', 'Супер ', 'Это ', 'А вот '];
@@ -28,9 +27,9 @@ function getRandomElementFromArray (array) {
   return array[getRandomInteger(0, array.length - 1)];
 }
 
-function users () {
+function getUsers () {
   const totallyRandomArray = [];
-
+  const users = [];
   function getUniqueRandomId(array) {
     const randomId = getRandomInteger(1, COUNT_MOCK_USERS * MAX_COMMENTS);
     if (!totallyRandomArray.includes(randomId)) {
@@ -42,7 +41,8 @@ function users () {
 
   function getRandomComments() {
     const comments = [];
-    for (let i = 1; i <= getRandomInteger(1, 3); i++) {
+    const totalCommentsCount = getRandomInteger(1, 3);
+    for (let i = 1; i <= totalCommentsCount; i++) {
       const comment = {
         id: getUniqueRandomId(totallyRandomArray),
         avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
@@ -62,9 +62,9 @@ function users () {
       likes: getRandomInteger(15, 200),
       comments: getRandomComments(),
     };
-    USERS.push(currentUser);
+    users.push(currentUser);
   }
-  return USERS;
+  return users;
 }
 
-users();
+getUsers();
