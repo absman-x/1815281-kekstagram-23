5c3d71a99b07f0964eb0fc6b849b9cb7a93fb927
absman-x <amc-x@yandex.ru>
2021-06-11 15:05:47 +0400

    Правки после замечаний

--- js/main.js
+++ js/main.js
@@ -16,58 +16,55 @@ function validateLength(text, validLength) {
 
 validateLength('тест', 140);
 
-function getFakeUsers () {
-  const fakeUsers = 25;
-  const maxComments = 3;
-  const fakeUsersArray = [];
-  const messageVariants = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-  const nameVariants = ['Дмитрий', 'Анатолий', 'Рушат', 'Андрей', 'Ольга', 'Евгений', 'Наталья', 'Вячеслав', 'Альфия'];
-  const descriptionFirstPart = ['Просто ', 'Супер ', 'Это ', 'А вот '];
-  const descriptionSecondPart = ['фотка', 'фото', 'картинка', 'пикча'];
+const COUNT_MOCK_USERS = 25;
+const MAX_COMMENTS = 3;
+const USERS = [];
+const MESSAGE_VARIANTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const NAME_VARIANTS = ['Дмитрий', 'Анатолий', 'Рушат', 'Андрей', 'Ольга', 'Евгений', 'Наталья', 'Вячеслав', 'Альфия'];
+const DESCRIPTION_FIRST_PARTS = ['Просто ', 'Супер ', 'Это ', 'А вот '];
+const DESCRIPTION_SECOND_PARTS = ['фотка', 'фото', 'картинка', 'пикча'];
+
+function getRandomElementFromArray (array) {
+  return array[getRandomInteger(0, array.length - 1)];
+}
+
+function users () {
   const totallyRandomArray = [];
 
-  function isInArray(array, element) {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === element) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
   function getUniqueRandomId(array) {
-    const randomId = getRandomInteger(1, fakeUsers * maxComments);
-    if (!isInArray(totallyRandomArray, randomId)) {
+    const randomId = getRandomInteger(1, COUNT_MOCK_USERS * MAX_COMMENTS);
+    if (!totallyRandomArray.includes(randomId)) {
       totallyRandomArray.push(randomId);
       return randomId;
     }
     return getUniqueRandomId(array);
   }
 
-  function getRandomComment() {
-    const commentArray = [];
-    for (let j = 1; j <= getRandomInteger(1, 3); j++) {
-      const comment = {};
-      comment.id = getUniqueRandomId(totallyRandomArray);
-      comment.avatar = `img/avatar-${getRandomInteger(1, 6)}.svg`;
-      comment.message = messageVariants[getRandomInteger(0, messageVariants.length - 1)];
-      comment.name = nameVariants[getRandomInteger(0, nameVariants.length - 1)];
-      commentArray.push(comment);
+  function getRandomComments() {
+    const comments = [];
+    for (let i = 1; i <= getRandomInteger(1, 3); i++) {
+      const comment = {
+        id: getUniqueRandomId(totallyRandomArray),
+        avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+        message: getRandomElementFromArray(MESSAGE_VARIANTS),
+        name: getRandomElementFromArray(NAME_VARIANTS),
+      };
+      comments.push(comment);
     }
-    return commentArray;
+    return comments;
   }
 
-  for (let i = 1; i <= fakeUsers; i++) {
-    const currentFakeUser = {};
-    currentFakeUser.id = i;
-    currentFakeUser.url = `photos/${i}.jpg`;
-    currentFakeUser.description = `${descriptionFirstPart[getRandomInteger(0, descriptionFirstPart.length - 1)]}${descriptionSecondPart[getRandomInteger(0, descriptionSecondPart.length - 1)]}`;
-    currentFakeUser.likes = getRandomInteger(15, 200);
-    currentFakeUser.comments = getRandomComment();
-    fakeUsersArray.push(currentFakeUser);
+  for (let i = 1; i <= COUNT_MOCK_USERS; i++) {
+    const currentUser = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: `${getRandomElementFromArray(DESCRIPTION_FIRST_PARTS)}${getRandomElementFromArray(DESCRIPTION_SECOND_PARTS)}`,
+      likes: getRandomInteger(15, 200),
+      comments: getRandomComments(),
+    };
+    USERS.push(currentUser);
   }
-  return fakeUsersArray;
+  return USERS;
 }
 
-getFakeUsers();
+users();
